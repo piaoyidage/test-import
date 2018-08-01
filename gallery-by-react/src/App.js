18efb894e798ac8d2ae7dfed79d934d60aa15a59
piaoyidage <longyixiaoge@gmail.com>
2018-08-01 16:03:53 +0800

    初步编写单个图片组件和图片画廊组件

--- gallery-by-react/src/App.js
+++ gallery-by-react/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import './App.css'
 
-// 读取数据描述
+// 读取图片数据描述
 let imageData = require('./data/imageData.json')
 
-// 给每条数据增加路径信息
+// 给每条图片数据增加路径信息
 function setImageUrl(imageArray) {
     const len = imageArray.length
     for (let i = 0; i < len; i += 1) {
@@ -15,15 +15,42 @@ function setImageUrl(imageArray) {
 
 imageData = setImageUrl(imageData)
 
-console.log(imageData)
+/**
+ * 单个图片
+ */
+class ImageFigure extends Component {
+    render() {
+        const { url, title } = this.props.data
+        return (
+            <figure className="figure-wrap">
+                <img className="figure-image" src={url} alt={title} />
+                <figcaption className="figure-title">
+                    {title}
+                </figcaption>
+            </figure>
+        )
+    }
+}
 
+/**
+ * 图片画廊
+ */
 class Gallery extends Component {
     render() {
+        const imageFigures = []
+        const controllerUnits = []
+
+        imageData.forEach(item => {
+            imageFigures.push(<ImageFigure key={item.url} data={item} />)
+        })
+
         return (
             <div className="stage">
                 <section className="image-area">
+                    {imageFigures}
                 </section>
                 <nav className="controller">
+                    {controllerUnits}
                 </nav>
             </div>
         )
