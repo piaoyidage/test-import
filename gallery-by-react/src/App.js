1e7867a6ada8fe384e3598f78de6b49fbff50e9a
piaoyidage <longyixiaoge@gmail.com>
2018-08-01 15:26:31 +0800

    为图片增加路径信息

--- gallery-by-react/src/App.js
+++ gallery-by-react/src/App.js
@@ -1,29 +1,33 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
-class App extends Component {
+// 读取数据描述
+let imageData = require('./data/imageData.json')
+
+// 给每条数据增加路径信息
+function setImageUrl(imageArray) {
+    const len = imageArray.length
+    for (let i = 0; i < len; i += 1) {
+        imageArray[i].url = require(`./images/${i + 1}.jpg`)
+    }
+    return imageArray
+}
+
+imageData = setImageUrl(imageData)
+
+console.log(imageData)
+
+class Gallery extends Component {
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">
-                        Welcome to React
-                    </h1>
-                </header>
-                <p className="App-intro">
-                    To get started, edit
-                    {' '}
-                    <code>
-                        src/App.js
-                    </code>
-                    {' '}
-                    and save to reload.
-                </p>
+            <div className="stage">
+                <section className="image-area">
+                </section>
+                <nav className="controller">
+                </nav>
             </div>
         )
     }
 }
 
-export default App
+export default Gallery
